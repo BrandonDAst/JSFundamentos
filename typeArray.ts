2a08ca3be596638cbda223cbaad4b2c2882b5f45
Brandon Diaz <brandon.diaz.as@gmail.com>
2020-08-04 22:25:44 -0500

    Arrays js ts

---
+++ typeArray.ts
@@ -0,0 +1,12 @@
+//Permite definir una variable para gestionar un conjunto de datos
+//Por corchetes []
+let users: string[];
+users = ['asdasd', 'hgfd', 'hgfd', 'rytu', 'dsa'];
+
+//Por interidos con clase Array
+let other = ['asdasd', 'hgfd', 'hgfd', 'rytu', 'dsa'];
+
+//Notacion array (generico)
+let pictureTitle: Array<string>;
+pictureTitle = ['asdasd', 'hgfd', 'hgfd', 'rytu', 'dsa'];
+
