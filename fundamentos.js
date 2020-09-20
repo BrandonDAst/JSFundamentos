110221eebf785a103465b6eacac2c1b538d96257
Brandon Diaz <brandon.diaz.as@gmail.com>
2020-09-20 18:11:30 -0500

    U2 Variables Numericos

--- fundamentos.js
+++ fundamentos.js
@@ -2,14 +2,32 @@ console.log("Curso de Fundamentos. Hola Mundo.")
 //////////////////////////////////////////////////////////////////
 // VARIABLES
 //////////////////////////////////////////////////////////////////
-var nombre = 'Brandon', apellido = 'Diaz', edad = 25
+var nombre = 'Brandon',
+    apellido = 'Diaz',
+    edad = 25,
+    peso = 80
 var nombreMayus = nombre.toUpperCase()
 var apellidoMinus = apellido.toLowerCase()
 var primeraLetra = nombre.charAt(0)
 var cantidadLetras = nombre.length
 var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
-var subStr = nombre.substr(1,3)
+var subStr = nombre.substr(1, 3)
 
 console.log('Hola ' + nombre + ' ' + apellido)
-console.log ('Tengo '+ edad + ' años')
-console.log(`Hola {nombre}`)
+
+// Numeros enteros
+edad = edad + 1
+edad += 1
+peso = peso - 1
+peso -= peso
+
+// Numeros con decimales
+var precioVino = 200.3
+var totalDecimal = precioVino * 100 * 3 / 100
+var totalRound = Math.round(precioVino * 100 * 3) / 100
+var totalDosDecimal = totalRound.toFixed(2) //String
+var totalDosDecimalNoString = parseFloat(totalDosDecimal)
+
+var pizza = 8
+var personas = 2
+var rebanadas = pizza / personas
