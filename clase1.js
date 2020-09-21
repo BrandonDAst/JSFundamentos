30d362fc8f5d59f356529f6db78fda1d985407d6
Brandon Diaz <brandon.diaz.as@gmail.com>
2020-09-21 12:58:57 -0500

    Limpieza de apuntes

--- clase1.js
+++
@@ -1,9 +0,0 @@
-var nombre = 'Brandon', apellido = 'Diaz'
-var nombreMayusculas = nombre.toUpperCase()
-var apellidoMinusculas = apellido.toLowerCase()
-var primeraNombre = nombre.charAt(0)
-var letrasNombre = nombre.length
-var nombreCompleto = nombre + ' ' + apellido
-var interpolacionTexto = `${nombre} ${apellido}`
-var subCadena = nombre.substr(1,2)
-var ultimaLetra = nombre.charAt(nombre.length-1)
