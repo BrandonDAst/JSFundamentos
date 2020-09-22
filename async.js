09bd499933d0ffc5cb36a9766de9b4cc52b25602
Brandon Diaz <brandon.diaz.as@gmail.com>
2020-09-22 00:16:18 -0500

    U6 Asincronismo - Async Await

--- async.js
+++ async.js
@@ -61,27 +61,55 @@ function ObtenerPersonajePromesa(id) {
     })
 }
 
-ObtenerPersonajePromesa(1)
-    .then((p1) => {
-        console.log(`El personaje 1 es ${p1.name}`)
-        return ObtenerPersonajePromesa(2)
-    })
-    .then((p2) => {
-        console.log(`El personaje 2 es ${p2.name}`)
-        return ObtenerPersonajePromesa(3)
-    })
-    .then((p3) => {
-        console.log(`El personaje 3 es ${p3.name}`)
-        return ObtenerPersonajePromesa(4)
-    })
-    .then((p4) => {
-        console.log(`El personaje 4 es ${p4.name}`)
-        return ObtenerPersonajePromesa(5)
-    })
-    .then((p5) => {
-        console.log(`El personaje 5 es ${p5.name}`)
-        //return ObtenerPersonajePromesa(4)
-    })
-    .catch((id) => {
+//Promesas en Serie
+// ObtenerPersonajePromesa(1)
+//     .then((p1) => {
+//         console.log(`El personaje 1 es ${p1.name}`)
+//         return ObtenerPersonajePromesa(2)
+//     })
+//     .then((p2) => {
+//         console.log(`El personaje 2 es ${p2.name}`)
+//         return ObtenerPersonajePromesa(3)
+//     })
+//     .then((p3) => {
+//         console.log(`El personaje 3 es ${p3.name}`)
+//         return ObtenerPersonajePromesa(4)
+//     })
+//     .then((p4) => {
+//         console.log(`El personaje 4 es ${p4.name}`)
+//         return ObtenerPersonajePromesa(5)
+//     })
+//     .then((p5) => {
+//         console.log(`El personaje 5 es ${p5.name}`)
+//         //return ObtenerPersonajePromesa(4)
+//     })
+//     .catch((id) => {
+//         console.log(`Sucedió un error al obtener el personaje ${id}`)
+//     })
+
+//Promesas en paralelo
+// var ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// var promesas = ids.map(id => ObtenerPersonajePromesa(id))
+
+// Promise
+//     .all(promesas)
+//     .then(personajes => console.log(personajes))
+//     .catch(id => {
+//         console.log(`Sucedió un error al obtener el personaje ${id}`)
+//     })
+
+// Async await
+async function ObtenerPersonajes() {
+    var ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    var promesas = ids.map(id => ObtenerPersonajePromesa(id))
+
+    try {
+        var personajes = await Promise.all(promesas)
+        console.log(personajes)
+    } catch (id) {
         console.log(`Sucedió un error al obtener el personaje ${id}`)
-    })
+    }
+}
+
+ObtenerPersonajes()
