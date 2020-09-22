5720c6d652345a385c3dc4c16df3320de1475612
Brandon Diaz <brandon.diaz.as@gmail.com>
2020-09-21 22:14:04 -0500

    U6 Asincronismo - Multiples Request

--- async.js
+++ async.js
@@ -8,6 +8,13 @@ const REQUEST_OPTIONS = {
 const ON_PEOPLE_RESPONSE = (person) => {
     console.log(`Hola, yo soy ${person.name}`)
 }
+//$.get(REQUEST_URL, REQUEST_OPTIONS, ON_PEOPLE_RESPONSE)
 
-//
-$.get(REQUEST_URL, REQUEST_OPTIONS, ON_PEOPLE_RESPONSE)
+function ObtenerPersonaje(id) {
+    const URL = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(URL, REQUEST_OPTIONS, ON_PEOPLE_RESPONSE)
+}
+
+ObtenerPersonaje(1)
+ObtenerPersonaje(2)
+ObtenerPersonaje(3)
