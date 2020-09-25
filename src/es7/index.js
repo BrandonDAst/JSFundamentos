b90b31b13e2a430a088e34c243223317ea81b3bc
Brandon Diaz <brandon.diaz.as@gmail.com>
2020-09-25 18:08:23 -0500

    Ecmascript 7

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+// Revisar si un arreglo incluye un elemento
+let numbers = [1, 3, 5, 2, 8, 5]
+
+if (numbers.includes(7)) {
+    console.log("Si se encuentra el valor 7")
+} else {
+    console.log("No se encuentra")
+}
+
+// Potencias 
+let base = 4
+let exponente = 3
+
+let resultado = base ** exponente
+console.log(resultado)
