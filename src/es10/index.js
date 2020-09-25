423d4a2f288296ebf2926061ba8e24f574b1d511
Brandon Diaz <brandon.diaz.as@gmail.com>
2020-09-25 18:54:31 -0500

    Ecmascript 10

---
+++ src/es10/index.js
@@ -0,0 +1,13 @@
+let array3 = [1, 2, 3, [4, 5, 6, [7, 8, 9]]];
+console.log(array3.flat(2))
+
+let array = [1, 2, 3, 4, 5]
+console.log(array.flatMap(value => [value, value * 2]));
+
+
+let str = '         Hello';
+console.log(str)
+console.log(str.trimStart())
+
+// Simbolos
+// Object from entries
